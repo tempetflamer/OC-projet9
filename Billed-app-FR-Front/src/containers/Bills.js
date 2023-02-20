37c8e8191c806c6bff641044ff0572f2888d873c
tempet <tempetflamer@gmail.com>
2023-02-20 10:29:34 +0100

    fix Bills [bug report] fix bug when the date are not ordered on the site, only the test pass convert date in Date format, change the sort method after performance test in benchmark, choice of (new Date(b.date) - new Date(a.date)) over (new Date(a.date) < new Date(b.date) ? 1 : new Date(a.date) > new Date(b.date) ? -1 : 0) Also change the test already writen in adding this comparing method of date

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -30,31 +30,30 @@ export default class {
   getBills = () => {
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot.sort((a, b) => (a.dates < b.dates ? 1 : a.dates > b.dates ? -1 : 0))
-          .map(doc => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status)
+        .bills()
+        .list()
+        .then(snapshot => {
+          const bills = snapshot.sort((a, b) => (new Date(b.date) - new Date(a.date)))
+          .map(doc => { 
+              try {
+                return {
+                  ...doc,
+                  date: formatDate(doc.date),
+                  status: formatStatus(doc.status)
+                }
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, 'for', doc)
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status)
+                }
               }
-            } catch(e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
-            }
-          })
-          console.log('length', bills.length)
-        return bills
-      })
+            })
+          return bills
+        })
     }
   }
 }
--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -20,7 +20,7 @@ const row = (bill) => {
   }
 
   const rows = (data) => {
-    return (data && data.length) ? data.sort((a, b) => ((a.date < b.date) ? 1 : (a.date > b.date) ? -1 : 0)).map(bill => row(bill)).join("") : ""
+    return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
   }
 
 export default ({ data: bills, loading, error }) => {
